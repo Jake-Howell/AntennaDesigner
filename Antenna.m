9bd50a3649fd1a90cfc4c242058cab2310504e90
GRPlym56 <64791542+GRPlym56@users.noreply.github.com>
2021-10-25 18:37:00 +0100

    Roots of A :)

--- Antenna.m
+++ Antenna.m
@@ -3,7 +3,6 @@ classdef Antenna
         Gain = 0;%just gain
         fc;            %cutoff frequency [lower, higher]
         C = 2.99792*10^8;     %speed of light
-        Vp = 0.95*C;          %velocity of propargation though copper
         efficiency = 0.5
         
         waveGuideDimensions;
@@ -11,16 +10,27 @@ classdef Antenna
     end
     methods
         function obj = Antenna(centerFreq, bandwidth)
+            Vp = 0.95*2.99792*10^8;  
             fc = [(centerFreq-(bandwidth/2)), (centerFreq+(bandwidth/2))];  %calculate upper and lower cut off frequencys
             waveGuideDimensions = [(Vp/(2*fc(2))),(Vp/(4*fc(2))), (Vp/fc(1))];          %calculate waveguide [width, height, length]
             
+            a = waveGuideDimensions(1);
+            b = a/2;
             
-            wavelengthFc = Vp/fc;
+            efficiency = 0.5;
+            waveLength = Vp/fc(1);
+            
+            waveLengthFc = [Vp/fc(1), Vp/fc(2)];
             apertureDimensions = []; %calculate aperture [width, height, length]
             
+            efficiency = 0.5;
+            Gain = 10;
+            
+            AQuartic = [1 -a  0 ((3*b*Gain*waveLengthFc(1)^2)/(8*pi*efficiency)) -((3*(Gain^2)*(waveLength(1)^4))/(32*(pi^2)*(efficiency^2)))]; 
             
             
-            A^4-a*A^3+((3*b*Gain*waveLengthFc(1)^2)/(8*pi*efficency))*A-((3*(Gain^2)*(waveLength(1)^4))/(32*(pi^2)*(efficiency^2)))
+            roots(AQuartic)
+            %A^4-a*A^3+((3*b*Gain*waveLengthFc(1)^2)/(8*pi*efficency))*A-((3*(Gain^2)*(waveLength(1)^4))/(32*(pi^2)*(efficiency^2)))
             
             
         end
